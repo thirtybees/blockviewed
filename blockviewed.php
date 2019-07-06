b06abde213a9ec417529fff7c3af7420593ee0d3
Petr Hučík <petr@getdatakick.com>
2019-07-06 10:40:54 +0200

    Release version 2.0.1

--- blockviewed.php
+++ blockviewed.php
@@ -44,7 +44,7 @@ class BlockViewed extends Module
     {
         $this->name = 'blockviewed';
         $this->tab = 'front_office_features';
-        $this->version = '2.0.0';
+        $this->version = '2.0.1';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
         $this->bootstrap = true;
